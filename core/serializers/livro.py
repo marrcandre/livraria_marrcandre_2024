6c4be56b1f286edf168191910fa3c1157be5a2d3
Marco André Mendes <marcoandre@gmail.com>
2024-04-25 15:59:59 -0300

    Atualiza modelo Livro para incluir campo capa

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,9 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from core.models import Livro
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 
 class LivroDetailSerializer(ModelSerializer):
+    capa = ImageSerializer(required=False)
+
     class Meta:
         model = Livro
         fields = "__all__"
@@ -17,6 +21,15 @@ class LivroListSerializer(ModelSerializer):
 
 
 class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Livro
         fields = "__all__"
