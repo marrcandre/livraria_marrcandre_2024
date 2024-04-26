8903be9e5af4fb1c3d6b5d07336435c446f42fd4
Marco André Mendes <marcoandre@gmail.com>
2024-04-26 14:47:15 -0300

    Carga de dados

--- core/models/autor.py
+++ core/models/autor.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Autor(models.Model):
-    nome = models.CharField(max_length=100, unique=True)
+    nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, blank=True, null=True)
 
     def __str__(self):
--- core/models/editora.py
+++ core/models/editora.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Editora(models.Model):
-    nome = models.CharField(max_length=100, unique=True)
+    nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     cidade = models.CharField(max_length=100, blank=True, null=True)
