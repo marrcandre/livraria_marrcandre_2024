2108b1a045ffed25ca4f1d8ddcab4d98b956059a
Marco André Mendes <marcoandre@gmail.com>
2024-05-02 13:57:10 -0300

    Endpoint para a listagem básica de compras

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views import (
     AutorViewSet,
     CategoriaViewSet,
+    CompraViewSet,
     EditoraViewSet,
     LivroViewSet,
     UserViewSet,
@@ -23,6 +24,7 @@ from uploader.router import router as uploader_router
 router = DefaultRouter()
 router.register(r"autores", AutorViewSet, basename="autores")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"compras", CompraViewSet, basename="compras")
 router.register(r"editoras", EditoraViewSet, basename="editoras")
 router.register(r"livros", LivroViewSet, basename="livros")
 router.register(r"usuarios", UserViewSet, basename="usuarios")
--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,3 +3,4 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
 from .livro import LivroDetailSerializer, LivroListSerializer, LivroSerializer
+from .compra import CompraSerializer
