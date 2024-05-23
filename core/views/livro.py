2d462b38c0f0726d055f2f558b9647aeeab92f66
Marco André Mendes <marcoandre@gmail.com>
2024-05-23 09:52:04 -0300

    feat: Adicionar filtro de busca por título na listagem de livros

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
@@ -8,8 +9,9 @@ from core.serializers import LivroDetailSerializer, LivroListSerializer, LivroSe
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ["categoria__descricao", "editora__nome"]
+    search_fields = ["titulo"]
 
     def get_serializer_class(self):
         if self.action == "list":
