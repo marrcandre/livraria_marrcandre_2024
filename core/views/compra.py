b21a1a82acce11220650049889b1ba99c77a4c4a
Marco André Mendes <marcoandre@gmail.com>
2024-10-10 09:18:33 -0300

    Adicionar endpoint para alterar preço do livro

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.db import transaction
 
 from django_filters.rest_framework import DjangoFilterBackend
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,5 +1,9 @@
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
@@ -21,3 +25,29 @@ class LivroViewSet(ModelViewSet):
         elif self.action == "retrieve":
             return LivroDetailSerializer
         return LivroSerializer
+
+    @action(detail=True, methods=["patch"], url_path="alterar-preco")
+    def alterar_preco(self, request, pk=None):
+        # Busca o livro pelo ID
+        livro = get_object_or_404(Livro, pk=pk)
+
+        # Obtém o novo preço do corpo da requisição
+        novo_preco = request.data.get("preco")
+
+        # Verifica se o preço foi fornecido e se é um número válido
+        if novo_preco is None:
+            return Response({"detail": "O preço é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            novo_preco = float(novo_preco)
+        except ValueError:
+            return Response({"detail": "O preço deve ser um número válido."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Atualiza o preço do livro e salva
+        livro.preco = novo_preco
+        livro.save()
+
+        # Retorna uma resposta de sucesso
+        return Response(
+            {"detail": f"Preço do livro '{livro.titulo}' atualizado para {livro.preco}."}, status=status.HTTP_200_OK
+        )
