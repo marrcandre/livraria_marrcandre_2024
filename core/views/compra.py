b8363de56cb82e28f0cab49a5c7343a33b67fdb0
Marco André Mendes <marcoandre@gmail.com>
2024-12-09 10:54:03 -0300

    Refatorar método de finalização de compra para simplificar a lógica de verificação de estoque e atualizar status

--- core/views/compra.py
+++ core/views/compra.py
@@ -42,63 +42,44 @@ class CompraViewSet(ModelViewSet):
 
     @action(detail=True, methods=["post"])
     def finalizar(self, request, pk=None):
-        # Recupera o objeto 'compra' usando self.get_object(), com base no pk fornecido.
         compra = self.get_object()
 
-        # Verifica se o status da compra é diferente de 'CARRINHO'.
-        # Se não for, a compra já foi finalizada e não pode ser finalizada novamente.
         if compra.status != Compra.StatusCompra.CARRINHO:
             return Response(
                 status=status.HTTP_400_BAD_REQUEST,
                 data={"status": "Compra já finalizada"},
             )
 
-        # Abre uma transação atômica para garantir que todas as operações no banco
-        # de dados ocorram de forma consistente (ou todas são salvas ou nenhuma).
         with transaction.atomic():
-            # Itera sobre todos os itens da compra.
             for item in compra.itens.all():
-
-                # Verifica se a quantidade de um item é maior que a quantidade disponível no estoque do livro.
                 if item.quantidade > item.livro.quantidade:
-                    # Se a quantidade solicitada for maior que o estoque disponível, retorna um erro.
                     return Response(
                         status=status.HTTP_400_BAD_REQUEST,
                         data={
-                            "status": "Quantidade insuficiente",  # Mensagem de erro
-                            "livro": item.livro.titulo,  # Informa qual livro tem estoque insuficiente
-                            "quantidade_disponivel": item.livro.quantidade,  # Mostra a quantidade disponível
+                            "status": "Quantidade insuficiente",
+                            "livro": item.livro.titulo,
+                            "quantidade_disponivel": item.livro.quantidade,
                         },
                     )
 
-                # Se o estoque for suficiente, subtrai a quantidade do item do estoque do livro.
                 item.livro.quantidade -= item.quantidade
-                # Salva as alterações no livro (atualiza o estoque no banco de dados).
                 item.livro.save()
 
-            # Após todos os itens serem processados e o estoque ser atualizado,
-            # atualiza o status da compra para 'FINALIZADO'.
             compra.status = Compra.StatusCompra.FINALIZADO
-            # Salva as alterações da compra no banco de dados.
             compra.save()
 
-        # Retorna uma resposta de sucesso indicando que a compra foi finalizada.
         return Response(status=status.HTTP_200_OK, data={"status": "Compra finalizada"})
 
     @action(detail=False, methods=["get"])
     def relatorio_vendas_mes(self, request):
-        # Define o início do mês atual
         agora = timezone.now()
         inicio_mes = agora.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
 
-        # Filtra as compras realizadas desde o início do mês até o presente momento
         compras = Compra.objects.filter(status=Compra.StatusCompra.FINALIZADO, data__gte=inicio_mes)
 
-        # Calcula o total de vendas e a quantidade de vendas
         total_vendas = sum(compra.total for compra in compras)
         quantidade_vendas = compras.count()
 
-        # Retorna o relatório
         return Response(
             {
                 "status": "Relatório de vendas deste mês",
