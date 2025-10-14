fc25a5caf467abf102897758570c0e49568a9a60
Marco André Mendes <marcoandre@gmail.com>
2025-10-14 15:10:38 -0300

    feat: adiciona parâmetros de paginação personalizados na classe CustomPagination

--- app/pagination.py
+++ app/pagination.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 
 
 class CustomPagination(pagination.PageNumberPagination):
+    page_size = 10  # tamanho padrão da página
+    page_size_query_param = 'page_size'  # permitir mudar via query parameter
+    max_page_size = 100  # tamanho máximo da página que pode ser solicitado
+
     def get_paginated_response(self, data):
         return Response({
             'page': self.page.number,
